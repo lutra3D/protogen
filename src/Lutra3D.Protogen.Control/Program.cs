64236c81624f00dc183fbe55cff1b0ac287ddd5a
Mylan719 <Mylan719@gmail.com>
2025-06-05 10:40:09 +0200

    loading a gif

--- src/Lutra3D.Protogen.Control/Program.cs
+++ src/Lutra3D.Protogen.Control/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RPiRgbLEDMatrix;
+using System.Runtime.InteropServices;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using Color = RPiRgbLEDMatrix.Color;
+using Image = SixLabors.ImageSharp.Image;
+using System.Reflection;
+
+using var matrix = new RGBLedMatrix(32, 2, 1);
+var canvas = matrix.CreateOffscreenCanvas();
+
+var gifBytes = await LoadGifAsync("neutral");
+using var image = Image.Load<Rgb24>(gifBytes);
+image.Mutate(o => o.Resize(canvas.Width, canvas.Height));
+
+var running = true;
+Console.CancelKeyPress += (s, e) =>
+{
+    running = false;
+    e.Cancel = true; // don't terminate, we need to dispose
+};
+
+var frame = -1;
+// preprocess frames to get delays and pixel buffers
+var frames = image.Frames
+    .Select(f => (
+        Pixels: f.DangerousTryGetSinglePixelMemory(out var memory) ? memory : throw new("Could not get pixel buffer"),
+        Delay: f.Metadata.GetGifMetadata().FrameDelay * 10
+    )).ToArray();
+
+// run until user presses Ctrl+C
+while (running)
+{
+    frame = (frame + 1) % frames.Length;
+
+    var data = MemoryMarshal.Cast<Rgb24, Color>(frames[frame].Pixels.Span);
+    canvas.SetPixels(0, 0, canvas.Width, canvas.Height, data);
+
+    matrix.SwapOnVsync(canvas);
+    Thread.Sleep(frames[frame].Delay);
+}
+
+
+async Task<byte[]> LoadGifAsync(string emotion, CancellationToken cancellationToken = default)
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    var resourceName = $"Lutra3D.Protogen.Control.Emotions.{emotion}.gif";
+
+    using var stream = assembly.GetManifestResourceStream(resourceName)
+        ?? throw new InvalidOperationException($"Could not load emotion {emotion}");
+    using var memoryStream = new MemoryStream();
+    await stream.CopyToAsync(memoryStream, cancellationToken);
+    return memoryStream.ToArray();
+}
