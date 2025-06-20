88580d80c6ef70c599455bc2c63cedea49729bdd
Mylan719 <Mylan719@gmail.com>
2025-06-20 02:07:48 +0200

    old demo back

--- src/Lutra3D.Protogen.Server/Services/NeoPixelRedrawHostedService.cs
+++ src/Lutra3D.Protogen.Server/Services/NeoPixelRedrawHostedService.cs
@@ -1,61 +1,24 @@
-﻿
-using Iot.Device.Ws28xx;
-using System;
-using System.Device.Spi;
+﻿using rpi_ws281x;
 using System.Drawing;
 
 namespace Lutra3D.Protogen.Server.Services;
 
 public class NeoPixelRedrawHostedService(ProtogenManager protogenManager) : BackgroundService()
 {
-    protected sealed override async Task ExecuteAsync(CancellationToken ct)
+    protected sealed override Task ExecuteAsync(CancellationToken ct)
     {
-        await Task.Yield(); //Give App chance to init
+        var settings = Settings.CreateDefaultSettings();
+        settings.Channels[0] = new Channel(24, 18, 128, false, StripType.WS2812_STRIP);
 
-        SpiConnectionSettings settings = new(0, 0)
+        using (var rpi = new WS281x(settings))
         {
-            ClockFrequency = 2_400_000,
-            Mode = SpiMode.Mode0,
-            DataBitLength = 8
-        };
-        using SpiDevice spi = SpiDevice.Create(settings);
+            //Set the color of the first LED of channel 0 to blue
+            rpi.SetLEDColor(0, 0, Color.Blue);
+            //Set the color of the second LED of channel 0 to red
+            rpi.SetLEDColor(0, 1, Color.Red);
 
-        var neo = new Ws2812b(spi, 24);
-
-        while (!ct.IsCancellationRequested)
-        {
-            Rainbow(neo, 24);
-            await Task.Delay(200);
+            rpi.Render();
         }
-
-        void Rainbow(Ws28xx neo, int count, int iterations = 1)
-        {
-            var img = neo.Image;
-            for (var i = 0; i < 255 * iterations; i++)
-            {
-                for (var j = 0; j < count; j++)
-                {
-                    img.SetPixel(j, 0, Wheel((i + j) & 255));
-                }
-
-                neo.Update();
-            }
-        }
-
-        Color Wheel(int WheelPos)
-        {
-            WheelPos = 255 - WheelPos;
-            if (WheelPos < 85)
-            {
-                return Color.FromArgb(255 - WheelPos * 3, 0, WheelPos * 3);
-            }
-            if (WheelPos < 170)
-            {
-                WheelPos -= 85;
-                return Color.FromArgb(0, WheelPos * 3, 255 - WheelPos * 3);
-            }
-            WheelPos -= 170;
-            return Color.FromArgb(WheelPos * 3, 255 - WheelPos * 3, 0);
-        }
+        return Task.CompletedTask;
     }
 }
