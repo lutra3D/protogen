52eda5f2682d1de10cb101e1b816f9d349cd5e05
Mylan719 <Mylan719@gmail.com>
2025-06-20 22:39:16 +0200

    Using old project to test

--- src/Lutra3D.Protogen.Control/Program.cs
+++ src/Lutra3D.Protogen.Control/Program.cs
@@ -1,65 +1,15 @@
-﻿using RPiRgbLEDMatrix;
-using System.Runtime.InteropServices;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-using Color = RPiRgbLEDMatrix.Color;
-using Image = SixLabors.ImageSharp.Image;
-using System.Reflection;
+﻿using rpi_ws281x;
+using System.Drawing;
 
+var settings = Settings.CreateDefaultSettings();
+settings.Channels[0] = new Channel(24, 18, 128, false, StripType.WS2812_STRIP);
 
-var gifBytes = await LoadGifAsync("neutral");
-using var image = Image.Load<Rgb24>(gifBytes);
-
-var options = new RGBLedMatrixOptions
+using (var rpi = new WS281x(settings))
 {
-    ChainLength=2,
-    Parallel = 1,
-    Cols = 64,
-    HardwareMapping = "adafruit-hat"
-};
+    //Set the color of the first LED of channel 0 to blue
+    rpi.SetLEDColor(0, 0, Color.Blue);
+    //Set the color of the second LED of channel 0 to red
+    rpi.SetLEDColor(0, 1, Color.Red);
 
-using var matrix = new RGBLedMatrix(options);
-var canvas = matrix.CreateOffscreenCanvas();
-
-image.Mutate(o => o.Resize(canvas.Width, canvas.Height));
-
-var running = true;
-Console.CancelKeyPress += (s, e) =>
-{
-    running = false;
-    e.Cancel = true; // don't terminate, we need to dispose
-};
-
-var frame = -1;
-// preprocess frames to get delays and pixel buffers
-var frames = image.Frames
-    .Select(f => (
-        Pixels: f.DangerousTryGetSinglePixelMemory(out var memory) ? memory : throw new("Could not get pixel buffer"),
-        Delay: f.Metadata.GetGifMetadata().FrameDelay * 10
-    )).ToArray();
-
-// run until user presses Ctrl+C
-while (running)
-{
-    frame = (frame + 1) % frames.Length;
-
-    var data = MemoryMarshal.Cast<Rgb24, Color>(frames[frame].Pixels.Span);
-    canvas.SetPixels(0, 0, canvas.Width, canvas.Height, data);
-
-    matrix.SwapOnVsync(canvas);
-    Thread.Sleep(frames[frame].Delay);
-}
-
-
-async Task<byte[]> LoadGifAsync(string emotion, CancellationToken cancellationToken = default)
-{
-    var assembly = Assembly.GetExecutingAssembly();
-    var resourceName = $"Lutra3D.Protogen.Control.Emotions.{emotion}.gif";
-
-    using var stream = assembly.GetManifestResourceStream(resourceName)
-        ?? throw new InvalidOperationException($"Could not load emotion {emotion}");
-    using var memoryStream = new MemoryStream();
-    await stream.CopyToAsync(memoryStream, cancellationToken);
-    return memoryStream.ToArray();
+    rpi.Render();
 }
